7c61d7f0c0ae5c7a6f06c0485c4e31374a617a82
James Halliday <mail@substack.net>
2013-03-30 23:53:07 -0700

    tear down with a revert

--- test/repo/teardown.sh
+++ test/repo/teardown.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if test $(basename $PWD)="repo" -a -d .git; then
-    git reset --hard $(git log | grep ^commit | tail -n1 | sed 's/^commit //')
+    git revert -n $(git log | grep ^commit | tail -n1 | sed 's/^commit //')
     rm -rf .git
 fi
