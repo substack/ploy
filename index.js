8776e374d3ae82a2f23a8264a6199bbb805f7900
James Halliday <mail@substack.net>
2013-04-03 02:41:50 -0700

    factored out spawnProcess into lib/spawn.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var path = require('path');
 var fs = require('fs');
 
 var clone = require('clone');
-var parseQuote = require('shell-quote').parse;
 var spawn = require('child_process').spawn;
+var spawnProcess = require('./lib/spawn');
 
 module.exports = function (opts) {
     if (!opts) opts = {};
@@ -226,38 +226,3 @@ Ploy.prototype.handle = function (req, res) {
         res.end();
     }
 };
-
-function spawnProcess (commit, env, cb) {
-    fs.readFile(path.join(commit.dir, 'package.json'), function (err, src) {
-        if (err && err.code === 'ENOENT') {
-            src = JSON.stringify({ scripts: { start: 'node server.js' } });
-        }
-        else if (err) return cb(err);
-        
-        try { var pkg = JSON.parse(src) }
-        catch (e) { return cb(e) }
-        
-        var start = pkg.scripts && pkg.scripts.start || 'node server.js';
-        if (typeof start === 'string') {
-            start = { 'index': start };
-        }
-        
-        Object.keys(start).forEach(function (key) {
-            var pEnv = clone(env);
-            pEnv.PORT = Math.floor(Math.random()*(Math.pow(2,16)-1024)+1024);
-            var host = (key === 'index' ? '' : key + '.') + commit.branch;
-            runCommands(host, start[key], pEnv);
-        });
-    });
-    
-    function runCommands (host, cmd, env) {
-        if (!Array.isArray(cmd)) cmd = parseQuote(cmd);
-        var ps = commit.spawn(cmd, { env: env });
-        ps.port = env.PORT;
-        ps.host = host;
-        ps.stdout.pipe(process.stdout, { end: false });
-        ps.stderr.pipe(process.stderr, { end: false });
-        ps.respawn = function () { runCommands(host, cmd, env) };
-        cb(null, ps);
-    }
-}
