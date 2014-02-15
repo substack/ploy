d5a974742b01fcc16152e04aaccc4aa75b716a29
James Halliday <mail@substack.net>
2014-02-15 08:50:17 -0800

    example https redirector

---
+++ example/bouncer.js
@@ -0,0 +1,17 @@
+module.exports = function (req, res, bounce) {
+    if (!req.connection.encrypted) {
+        for (var i = 0; i < this.bouncers.length; i++) {
+            if (!this.bouncers[i].key) continue;
+            var hostname = req.headers.host.split(':')[0];
+            var host = hostname + ':' + this.bouncers[i].address().port;
+            
+            res.statusCode = 303;
+            res.setHeader('location', 'https://' + host + req.url);
+            return res.end();
+        }
+        
+        res.statusCode = 404;
+        res.end('no https endpoint configured\n');
+    }
+    else bounce()
+};
