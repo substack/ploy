7851acb778eea87b5f5f4f5a5885049fe6928f7f
Parsha Pourkhomami <parshap+git@gmail.com>
2015-01-08 17:27:32 -0800

    Add tests for skipTest option
    Use test/test.js as a starting point. This uses the test/source/test
    source which uses its tests to write file.txt. When using skipTest
    file.txt is never created.
    
    Make test/source/test/server.js read from file.txt only if it exists,
    otherwise the server will simply have no response body.
    
    Add test/skip-test.js which is the same thing as test/test.js except it
    passes the skipTest option to ploy() and it asserts an empty response
    from the server instead of the contents of file.txt (since file.txt was
    never created since tests were skipped).

--- test/source/test/server.js
+++ test/source/test/server.js
@@ -1,6 +1,15 @@
 var http = require('http');
 var fs = require('fs');
-var src = fs.readFileSync(__dirname + '/file.txt');
+
+var src;
+try {
+  src = fs.readFileSync(__dirname + '/file.txt');
+}
+catch (e) {
+  if (e.code !== "ENOENT") {
+    throw e;
+  }
+}
 
 var server = http.createServer(function (req, res) {
     res.end(src);
